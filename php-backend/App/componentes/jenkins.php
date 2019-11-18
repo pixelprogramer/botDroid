157196e168ffbf2f1f15b91af2918c6a2715758b
Nelson Andres Guerrero Moncada <nelson.guerrero@virtualplaza.net>
2019-11-17 21:01:04 -0500

    Prueba commit jenkins

---
+++ php-backend/App/componentes/jenkins.php
@@ -0,0 +1,13 @@
+<?php
+$app->get('/pruebaJenkins', function () use ($app) {
+
+    $helper = new helper();
+    $data = [
+        'status'=>'exitoso',
+        'code' => '000',
+        'msg' => 'Esto es una prueba de Jenkins'
+    ];
+    echo $helper->checkCode($data);
+});
+
+
