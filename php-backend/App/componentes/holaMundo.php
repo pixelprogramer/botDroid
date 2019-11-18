4050656135dd1f622fb4c4f81929ebccbe259e01
Nelson Andres Guerrero Moncada <nelson.guerrero@virtualplaza.net>
2019-11-17 19:58:46 -0500

    prueba jenkins

--- php-backend/App/componentes/holaMundo.php
+++ php-backend/App/componentes/holaMundo.php
@@ -32,6 +32,7 @@ $app->get('/holaMundoNikolaLuxember', function () use ($app) {
     echo $helper->checkCode($data);
 });
 
+
 $app->get('/holaMundoEulicer', function () use ($app) {
 
     $helper = new helper();
