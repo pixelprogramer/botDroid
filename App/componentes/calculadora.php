8306d60b9454291da58f964b44132f26dbf64767
Nelson Andres Guerrero Moncada <nelson.guerrero@virtualplaza.net>
2019-12-13 11:40:06 -0500

    test

--- App/componentes/calculadora.php
+++ App/componentes/calculadora.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\componentes;
+
 class calculadora {
     public function suma($n,$b){
         return $n+$b;
--- tests/holaMundoTest.php
+++ tests/holaMundoTest.php
@@ -1,15 +1,15 @@
 <?php
 
-use calculadora;
+use App\componentes\calculadora;
 
 class holaMundoTest extends \PHPUnit\Framework\TestCase
 {
     public function testSuma()
     {
-
+    $calculadora = new calculadora();
         $this->assertEquals(
             3,
-            3
+            $calculadora->suma(2,2)
         );
     }
 }
